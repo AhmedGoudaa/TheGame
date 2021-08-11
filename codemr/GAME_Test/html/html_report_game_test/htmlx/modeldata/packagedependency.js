a5960f26529ca59b624165af89695aaddb3c7037
AhmedGoudaa <ahmed.gouda@tajawal.com>
2021-08-11 23:23:49 +0400

    first comm

---
+++ codemr/GAME_Test/html/html_report_game_test/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,47 @@
+var matrix = [[0,1,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,2,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,1,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,4,0,0,0,0,5,7,5,0,0,0,0,0,7],[1,0,0,1,1,0,0,0,1,1,0,0,0,0,0],[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],[0,2,0,0,0,0,0,0,0,0,0,0,0,0,0],[0,2,0,0,2,0,0,0,2,0,0,0,0,0,0],[0,4,0,0,4,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,1,0,0,0,0,0,1,2],[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],[2,3,0,0,0,0,3,5,1,0,0,6,0,0,5],[0,3,0,0,0,0,0,7,0,0,0,0,0,0,0]]
+var packages = [{
+"name": " com.xgame.service", "color": " #3182bd"
+}
+,{
+"name": " com.xgame.entity", "color": " #6baed6"
+}
+,{
+"name": " com.xgame.comman.imp", "color": " #9ecae1"
+}
+,{
+"name": " com.xgame.service.impl", "color": " #c6dbef"
+}
+,{
+"name": " com.xgame.repository.dataStore", "color": " #e6550d"
+}
+,{
+"name": " com.xgame.handler", "color": " #fd8d3c"
+}
+,{
+"name": " com.xgame.factory", "color": " #fdae6b"
+}
+,{
+"name": " com.xgame.comman", "color": " #fdd0a2"
+}
+,{
+"name": " com.xgame.repository", "color": " #31a354"
+}
+,{
+"name": " com.xgame.repository.impl", "color": " #74c476"
+}
+,{
+"name": " com.xgame.repository.dataStore.impl", "color": " #a1d99b"
+}
+,{
+"name": " com.xgame", "color": " #c7e9c0"
+}
+,{
+"name": " com.xgame.exceptions", "color": " #756bb1"
+}
+,{
+"name": " com.xgame.command", "color": " #9e9ac8"
+}
+,{
+"name": " com.xgame.message", "color": " #bcbddc"
+}
+];
---
+++ codemr/GAME_Test/html/workingsets.js
@@ -0,0 +1,3 @@
+var EQ_workingSetList = [
+{name: 'GAME_Test', path:'game_test'}
+];
